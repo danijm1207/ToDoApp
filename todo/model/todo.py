e44e7f13c58a117b99b4e91c9ec52955284c078d
B12_309_21\b12s309 <julioochoamedrano@gmail.com>
2024-08-22 11:04:27 -0500

    cree el metodo Todo con sus clases y el TodoBook con su primera clase

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,38 @@
 # TODO: Add code here
+
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tag:
+            self.tags.append(tag)
+
+    def __str__(self) -> str:
+        return f"{self.code_id}-{self.title}"
+
+class TodoBook:
+
+    def __init__(self, todos: dict):
+        self.todos: dict = {}
+
+    def add_todo(self, title: str, description: str) ->:
+        new_id = len(self.todos) + 1
+
+        return
+
+
+
+
+
+
+
+
+
